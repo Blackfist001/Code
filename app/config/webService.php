06f6c058ceb0c6a6d1f54551750b0eafb6dad744
Blackfist696 <57995425+Blackfist696@users.noreply.github.com>
2026-04-24 16:09:58 +0200

    security: exclure fichiers de config sensibles du dépôt (webService.php, oauth_credentials.php, .webp)

--- app/config/webService.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-*   Config pour le web service de synchronisation des présences
-*/
-return [
-    'serviceWeb_password' => 'jiEK-t*p.R7;MZ5',
-    'profil_name' => 'Serveur_présences',
-    'profil_password' => '3debd4de2017ab608ac9',
-];
